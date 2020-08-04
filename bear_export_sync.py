99ee7398566f4243eb53ff79d36dda60fcecf49e
Zahan Malkani <zahanm@fb.com>
2020-08-03 21:15:29 -0700

    Restore checking hide_tags_in_comment_block in restore_tags()

--- bear_export_sync.py
+++ bear_export_sync.py
@@ -351,10 +351,10 @@ def hide_tags(md_text):
 
 def restore_tags(md_text):
     # Tags back to normal Bear tags, stripping the `period+space` at start of line:
-    # if hide_tags_in_comment_block:
-    md_text =  re.sub(r'(\n)<!--[ \t]*(\#[^\s#].*?) -->', r'\1\2', md_text)
-    # else:
-    md_text =  re.sub(r'(\n)\.[ \t]*(\#[^\s#]+)', r'\1\2', md_text)
+    if hide_tags_in_comment_block:
+        md_text =  re.sub(r'(\n)<!--[ \t]*(\#[^\s#].*?) -->', r'\1\2', md_text)
+    else:
+        md_text =  re.sub(r'(\n)\.[ \t]*(\#[^\s#]+)', r'\1\2', md_text)
     return md_text
 
 
